4b0b00945ed87fcb169b5aaee55519859be2a24b
merve.ugursac@sys.com.tr <merve.ugursac@sys.com.tr>
2021-10-19 21:39:02 +0300

    19 Ekimj ders sonu

---
+++ src/app.js
@@ -0,0 +1,16 @@
+console.log("Merhaba Merve");
+
+let dolarDun = 9.20;
+let dolarBugun = 9.30;
+console.log(dolarDun);
+
+// array
+// camelCasing : fields, function
+// PascalCasing
+let konutKredileri = ["Konut kredisi", "emlak konut kredisi", "Kamu konut kredisi"];
+
+for(let i=0; i<konutKredileri.length; i++){
+    
+}
+
+console.log(konutKredileri);
